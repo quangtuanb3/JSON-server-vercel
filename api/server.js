b237619abfdc20ebbad5d46d6b271df4d8ff91ea
quangtuanb3 <quangtuanb3@gmail.com>
2023-10-23 22:10:21 +0700

    try this

--- api/server.js
+++ api/server.js
@@ -1,32 +1,89 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+// // See https://github.com/typicode/json-server#module
+// const jsonServer = require('json-server')
 
-const server = jsonServer.create()
+// const server = jsonServer.create()
 
-// Uncomment to allow write operations
+// // Uncomment to allow write operations
 
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
 
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+// // Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults()
+// const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+// server.use(middlewares)
+// // Add this before server.use(router)
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/blog/:resource/:id/show': '/:resource/:id'
+// }))
+// server.use(router)
+// server.listen(3000, () => {
+//     console.log('JSON Server is running')
+// })
 
-// Export the Server API
-module.exports = server
+// // Export the Server API
+// module.exports = server
+
+// api/server.js
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+
+// Custom routes for CRUD operations
+server.post('/products', (req, res, next) => {
+    // Assuming req.body contains the new product object
+    const newProduct = req.body;
+    // Add the new product to the database
+    router.db.get('products').push(newProduct).write();
+    res.json(newProduct);
+});
+
+server.get('/products', (req, res, next) => {
+    // Retrieve all products from the database
+    const products = router.db.get('products').value();
+    res.json(products);
+});
+
+server.put('/products/:id', (req, res, next) => {
+    // Assuming req.body contains the updated product object
+    const updatedProduct = req.body;
+    // Update the product with the specified ID in the database
+    router.db.get('products').find({ id: req.params.id }).assign(updatedProduct).write();
+    res.json(updatedProduct);
+});
+
+server.patch('/products/:id', (req, res, next) => {
+    // Assuming req.body contains the specific fields to update
+    const updatedFields = req.body;
+    // Update specific attributes of the product with the specified ID in the database
+    router.db.get('products').find({ id: req.params.id }).assign(updatedFields).write();
+    res.json(router.db.get('products').find({ id: req.params.id }).value());
+});
+
+server.delete('/products/:id', (req, res, next) => {
+    // Delete the product with the specified ID from the database
+    router.db.get('products').remove({ id: req.params.id }).write();
+    res.sendStatus(200);
+});
+
+// Use the custom routes along with the JSON Server router
+server.use(router);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
+});
+
